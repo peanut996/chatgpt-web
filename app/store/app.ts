0a15cd6774e23e470b2dd01bbbfe92eaeb1e644d
peanut996 <849421294godw@gmail.com>
2023-04-11 10:12:38 +0800

    [refactor] revert gpt4 model.

--- app/store/app.ts
+++ app/store/app.ts
@@ -65,6 +65,11 @@ export const ALL_MODELS = [
     model: "text-davinci-002-render-paid",
     available: true,
   },
+  {
+    name: "GPT-4",
+    model: "gpt-4",
+    available: true,
+  },
 ];
 
 export function isValidModel(name: string) {
