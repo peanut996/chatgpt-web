d32946db8de0d9c504fb8f072b0f9b65b5780031
peanut996 <849421294godw@gmail.com>
2023-05-19 16:05:58 +0800

    [feat] remove access control.

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,8 +29,7 @@ export const metadata = {
 function Meta() {
   const metas = {
     version: COMMIT_ID ?? "unknown",
-    access:
-      ACCESS_CODES.size > 0 || IS_IN_DOCKER || SALT ? "enabled" : "disabled",
+    access: false,
   };
 
   return (
